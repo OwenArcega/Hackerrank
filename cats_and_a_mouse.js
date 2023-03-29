6c7f76fde8ce5cc49cf7bbde363a3dcbcbd05b92
OwenArcega <89950248+OwenArcega@users.noreply.github.com>
2023-03-28 21:49:42 -0600

    cats_and_a_mouse

---
+++ cats_and_a_mouse.js
@@ -0,0 +1,13 @@
+//Owen Arcega Guerrero
+function catAndMouse(x, y, z) {
+  if(Math.abs(x-z) < Math.abs(y-z)){
+    return "Cat A";
+  } else if(Math.abs(x-z) > Math.abs(y-z)){
+    return "Cat B";
+  } else if(Math.abs(x-z) == Math.abs(y-z)){
+    return "Mouse C";
+  }
+}
+
+console.log(catAndMouse(1,2,3));
+console.log(catAndMouse(1,3,2));
